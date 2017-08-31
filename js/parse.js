9b983e1dcbe26b7fc200902a438f0b6f12f0952d
prashant28797 <prashant28797@gmail.com>
2017-08-31 10:43:33 +0530

    updated files

--- js/parse.js
+++ js/parse.js
@@ -3,7 +3,9 @@ var lineReader = require('readline').createInterface({
   input: fs.createReadStream('./da.csv')
 });
 // json for sorted of population column
+
 var obj=[];
+
 lineReader.on('line',function(line){
 		var jsonFromLine={};
 		var lineSplit=line.split(',');
@@ -11,6 +13,8 @@ lineReader.on('line',function(line){
 		 jsonFromLine.population = lineSplit[5];
 		 obj.push(jsonFromLine);
 });
+//console.log(obj1);
+
 lineReader.on('close',function(line){
 var s=obj.filter(function(data){
 		if(data.CountryName != 'European Union' && data.CountryName != 'World')
@@ -23,8 +27,9 @@ console.log(s);
 fs.createWriteStream('./population.json').write(JSON.stringify(s,null,2));
 })
 
-//sorting of  gdp of country
+//sorting of  gdp pf country
 var gdp=[];
+
 lineReader.on('line',function(line){
 		var jsonFromLine={};
 		var lineSplit=line.split(',');
@@ -33,6 +38,8 @@ lineReader.on('line',function(line){
 		 gdp.push(jsonFromLine);
 
 });
+
+
 lineReader.on('close',function(line){
 var getdata1=gdp.filter(function(data){
 		if(data.CountryName != 'European Union' && data.CountryName != 'World')
@@ -46,6 +53,7 @@ fs.createWriteStream('./gdp.json').write(JSON.stringify(getdata1,null,2));
 
 //Sorting purchasing power
 var purchasing=[];
+
 lineReader.on('line',function(line){
 		var jsonFromLine={};
 		var lineSplit=line.split(',');
@@ -54,6 +62,7 @@ lineReader.on('line',function(line){
 		 purchasing.push(jsonFromLine);
 
 });
+
 lineReader.on('close',function(line){
 	var getdata2=purchasing.filter(function(data){
 		if(data.CountryName != 'European Union' && data.CountryName != 'World')
@@ -64,6 +73,7 @@ lineReader.on('close',function(line){
 	getdata2.shift();
 fs.createWriteStream('./purchasing.json').write(JSON.stringify(getdata2,null,2));
 });
+
 ///growth in popualtion in 2010 to 2013
 var growth=[];
 lineReader.on('line',function(line){
@@ -74,15 +84,19 @@ lineReader.on('line',function(line){
 	jsonFromLine.Population2013=lineSplit[5];
 	jsonFromLine.purchasingpower2010=lineSplit[14];
 	jsonFromLine.purchasingpower2013=lineSplit[17];
+
 growth.push(jsonFromLine);
+console.log(growth);
+
 })
+
 lineReader.on('close',function(line){
-var getgrowth=growth.filter(function(data){
+var l=growth.filter(function(data){
 		if(data.CountryName != 'European Union' && data.CountryName != 'World')
 		{
         return data.CountryName;
 		}
 	});
-getgrowth.shift();
-fs.createWriteStream("growth.json").write(JSON.stringify(getgrowth,null,2));
+l.shift();
+fs.createWriteStream("growth.json").write(JSON.stringify(l,null,2));
 });
